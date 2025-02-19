36f1c0136bbcf1825f2f97b56d58289cfef914b0
jiwan kim <jiwan.kim@kaonbroadband.com>
2025-02-19 17:41:03 +0900

    add : path alias for src/*

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,30 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
-const CUSTOM = require('./env.json')
-process.env.STATE = CUSTOM.STATE
-process.env.CRYPTO_SERVERADDRESS = CUSTOM.CRYPTO_SERVERADDRESS
-process.env.UPBIT_OPEN_API_ACCESS_KEY = CUSTOM.UPBIT_OPEN_API_ACCESS_KEY
-process.env.UPBIT_OPEN_API_SECRET_KEY = CUSTOM.UPBIT_OPEN_API_SECRET_KEY
-process.env.UPBIT_OPEN_API_SERVER_URL = CUSTOM.UPBIT_OPEN_API_SERVER_URL
+const CUSTOM = require('./env.json');
+process.env.STATE = CUSTOM.STATE;
+process.env.CRYPTO_SERVERADDRESS = CUSTOM.CRYPTO_SERVERADDRESS;
+process.env.UPBIT_OPEN_API_ACCESS_KEY = CUSTOM.UPBIT_OPEN_API_ACCESS_KEY;
+process.env.UPBIT_OPEN_API_SECRET_KEY = CUSTOM.UPBIT_OPEN_API_SECRET_KEY;
+process.env.UPBIT_OPEN_API_SERVER_URL = CUSTOM.UPBIT_OPEN_API_SERVER_URL;
 
 module.exports = defineConfig({
     outputDir: 'docs', // 빌드 결과물을 docs 디렉토리에 생성
     publicPath: './',  // 상대 경로로 설정하여 GitHub Pages와 같은 정적 서버에서 문제 방지
-    lintOnSave:false,
-	devServer: {
+    lintOnSave: false,
+    devServer: {
         client: {
             overlay: false
         }
-	},
+    },
     transpileDependencies: true,
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src')
+            }
+        }
+    },
     chainWebpack: (config) => {
         config.plugin('define').tap((args) => {
             const webpackConfig = args[0];
@@ -32,9 +40,9 @@ module.exports = defineConfig({
                 UPBIT_OPEN_API_ACCESS_KEY: JSON.stringify(process.env.UPBIT_OPEN_API_ACCESS_KEY),
                 UPBIT_OPEN_API_SECRET_KEY: JSON.stringify(process.env.UPBIT_OPEN_API_SECRET_KEY),
                 UPBIT_OPEN_API_SERVER_URL: JSON.stringify(process.env.UPBIT_OPEN_API_SERVER_URL)
-            }
+            };
 
             return args;
         });
     }
-})
+});
