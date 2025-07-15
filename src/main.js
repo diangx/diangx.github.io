650ab3bfc17f21b6f4083cce374741c409e4d53e
jiwan kim <jiwan.kim@kaonbroadband.com>
2025-07-15 20:51:47 +0900

    add : hilight code for code card pretty

--- src/main.js
+++ src/main.js
@@ -6,14 +6,33 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
+import hljs from 'highlight.js/lib/core'
+import javascript from 'highlight.js/lib/languages/javascript'
+import 'highlight.js/styles/github.css'
+
 import "./assets/global.css"
+
+hljs.registerLanguage('javascript', javascript)
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
+Vue.directive('highlight', {
+    inserted(el) {
+        el.querySelectorAll('pre code').forEach(block => {
+            hljs.highlightBlock(block)
+        })
+    },
+    componentUpdated(el) {
+        el.querySelectorAll('pre code').forEach(block => {
+            hljs.highlightBlock(block)
+        })
+    }
+})
+
 new Vue({
     router,
-    render: (h) => h(App),
-}).$mount("#app");
+    render: h => h(App),
+}).$mount("#app")
