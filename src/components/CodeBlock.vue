480e1c22450488be8ae1d3a2b83d42e4a87de97f
jiwan kim <jiwan.kim@kaonbroadband.com>
2025-07-16 11:17:29 +0900

    add : code block components

---
+++ src/components/CodeBlock.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-highlight>
+        <pre class="bg-light border rounded overflow-auto text-start m-0"><code :class="['font-monospace', `language-${language}`]" v-text="codeContent"></code></pre>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CodeBlock',
+    props: {
+        codeContent: {
+            type: String,
+            required: true
+        },
+        language: {
+            type: String,
+            default: 'javascript'
+        }
+    }
+}
+</script>
